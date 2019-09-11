8a6fbb62988f0a77981d764f017039ce6df3ea30
CScori <amilanotte1366@gmail.com>
2019-09-11 16:09:10 -0500

    adds number export and maping data

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { numbers } from './../../../data.js'
+import NumberButton from './NumberButton'
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
 /* 
@@ -7,13 +8,15 @@ import { numbers } from '../../../data'
 */
 //Import your array data to from the provided data file
 
-const Numbers = () => {
-  // STEP 2 - add the imported data to state
+const Numbers = ({ updateValue }) => {
+  const [numbersData ] = useState(numbers)
+ 
+  
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+      {numbersData.map((number, idx) => <NumberButton key={idx} number={number} clickNumberBtn={() => updateValue(number)} />)
+      })
     </div>
   );
-};
+    }
+ export default Numbers
